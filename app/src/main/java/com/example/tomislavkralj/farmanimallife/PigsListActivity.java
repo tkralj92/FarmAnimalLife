0634ee3db1b544e997aab4bf2e19a6eeb43563dc
Tomislav <kraljtomislav92@gmail.com>
2017-07-11 20:51:21 +0200

    11.07 gotovo za danas

--- app/src/main/java/com/example/tomislavkralj/farmanimallife/PigsListActivity.java
+++ app/src/main/java/com/example/tomislavkralj/farmanimallife/PigsListActivity.java
@@ -1,6 +1,5 @@
 package com.example.tomislavkralj.farmanimallife;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -8,7 +7,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewParent;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
